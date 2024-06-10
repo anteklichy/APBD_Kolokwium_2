5437106f759f6645eab37b6ebb97ea0aaad05ac2
Antek <s27056@pjwstk.edu.pl>
2024-06-10 09:46:42 +0200

    possibly final

--- Dtos/ClientDto.cs
+++ Dtos/ClientDto.cs
@@ -6,6 +6,6 @@ public class ClientDto
     public string FirstName { get; set; }
     public string LastName { get; set; }
     public string Email { get; set; }
-    public string Phone { get; set; }
-    public List<SubscriptionDto> Subscriptions { get; set; }
+    public string? Phone { get; set; }
+    public List<SubscriptionDto>? Subscriptions { get; set; }
 }
